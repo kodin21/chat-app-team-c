f9553967ff83ffc9185e461b94183bba4a15f252
PDOUU <58252790+pdouu@users.noreply.github.com>
2021-08-11 18:18:30 +0300

    create language context

--- src/context/ThemeContext.js
+++ src/context/ThemeContext.js
@@ -1,4 +1,5 @@
 import { createContext, useContext, useState } from "react";
+import useLocalStorage from "../hooks/useLocalStorage";
 
 const ThemeContext = createContext();
 
@@ -7,9 +8,10 @@ function useTheme() {
 }
 
 function ThemeProvider({ children }) {
-  const [appTheme, setTheme] = useState("retro");
+  const [appTheme, setTheme] = useLocalStorage("theme", "retro");
+  const [language, setLanguage] = useLocalStorage("language", "en");
 
-  const value = { appTheme, setTheme };
+  const value = { appTheme, setTheme, language, setLanguage };
 
   return (
     <ThemeContext.Provider value={value}>{children}</ThemeContext.Provider>
