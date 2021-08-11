d55c8005f1e0b22ac0f465d3d9a7636959cd8cc5
PDOUU <58252790+pdouu@users.noreply.github.com>
2021-08-11 16:55:12 +0300

    implement routing, theme context

--- src/App.js
+++ src/App.js
@@ -1,11 +1,27 @@
-import "./App.scss";
+import { useTheme } from "./context/ThemeContext";
+import { Switch, Route, Redirect } from "react-router-dom";
 
-import ModalWindow from "./components/ModalWindow/ModalWindow";
+import "./App.scss";
+import Login from "./pages/login/Login";
+import Rooms from "./pages/rooms/Rooms";
 
 function App() {
+  //Theme Context
+  const { appTheme } = useTheme();
+
   return (
-    <div className="app app__background--retro">
-      <ModalWindow />
+    <div className={`app app__background--${appTheme}`}>
+      <Switch>
+        <Route path="/login">
+          <Login />
+        </Route>
+        <Route path="/rooms">
+          <Rooms />
+        </Route>
+        <Route>
+          <Redirect to="/login" />
+        </Route>
+      </Switch>
     </div>
   );
 }
