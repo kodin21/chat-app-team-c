049f77f4ade00d5dc89e931ec69d8ff34d669f8e
PDOUU <58252790+pdouu@users.noreply.github.com>
2021-08-11 21:22:16 +0300

    change database to firestore

--- src/firebase/firebaseUtils.js
+++ src/firebase/firebaseUtils.js
@@ -1,9 +1,9 @@
 import firebase from "firebase/app";
-import "firebase/database";
+import "firebase/firestore";
 import firebaseConfig from "./firebaseConfig";
 
 firebase.initializeApp(firebaseConfig);
 
-export const database = firebase.database();
+export const database = firebase.firestore();
 
 export default firebase;
