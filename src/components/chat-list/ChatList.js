a3f50ddd440c058871da9083077cf02cb80b1d6e
PDOUU <58252790+pdouu@users.noreply.github.com>
2021-08-12 14:28:01 +0300

    get chat messages

--- src/components/chat-list/ChatList.js
+++ src/components/chat-list/ChatList.js
@@ -1,14 +1,28 @@
-import React from "react";
+import React, { useEffect } from "react";
+import { useCollection, useDocument } from "react-firebase-hooks/firestore";
 import { useTheme } from "../../context/ThemeContext";
+import { database } from "../../firebase/firebaseUtils";
 import Loading from "../loading/Loading";
 
 import "./ChatList.style.scss";
 
-export default function ChatList({ chatList }) {
+export default function ChatList({ chatID }) {
   const { appTheme } = useTheme();
+
+  const [value, loading, error] = useDocument(
+    database.collection("messages").doc(chatID)
+  );
+
+  useEffect(() => {
+    if (value) {
+      console.log(chatID);
+    }
+  }, [value]);
+
   return (
     <div className={`chat-list chat-list--${appTheme}`}>
-      {/* <Loading /> */}
+      {value &&
+        Object.keys(value.data()).map((key) => <p>{value.data()[key].text}</p>)}
     </div>
   );
 }
