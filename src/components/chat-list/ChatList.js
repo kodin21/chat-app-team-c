7fa1e4f78ba6cd10a3dc379b18406eed1820a0a4
Onur Yüksekkaya <sosyal@onuryuksekkaya.com>
2021-08-12 16:40:48 +0300

    uptade chatlist

--- src/components/chat-list/ChatList.js
+++ src/components/chat-list/ChatList.js
@@ -1,4 +1,4 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import { useCollection, useDocument } from "react-firebase-hooks/firestore";
 import { useTheme } from "../../context/ThemeContext";
 import { database } from "../../firebase/firebaseUtils";
@@ -8,21 +8,17 @@ import "./ChatList.style.scss";
 
 export default function ChatList({ chatID }) {
   const { appTheme } = useTheme();
+  const [messages, setMessages] = useState([]);
 
   const [value, loading, error] = useDocument(
-    database.collection("messages").doc(chatID)
+    database.collection(`room-list/${chatID}/messages`)
   );
 
   useEffect(() => {
     if (value) {
-      console.log(chatID);
+      console.log(value.docs.map((doc) => doc.data()));
     }
   }, [value]);
 
-  return (
-    <div className={`chat-list chat-list--${appTheme}`}>
-      {value &&
-        Object.keys(value.data()).map((key) => <p>{value.data()[key].text}</p>)}
-    </div>
-  );
+  return <div className={`chat-list chat-list--${appTheme}`}></div>;
 }
