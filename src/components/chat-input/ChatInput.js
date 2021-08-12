55da0a38bcd46fd355663fd8210b284ca343ed8d
PDOUU <58252790+pdouu@users.noreply.github.com>
2021-08-12 19:58:18 +0300

    move input functions to ChatInput

--- src/components/chat-input/ChatInput.js
+++ src/components/chat-input/ChatInput.js
@@ -1,15 +1,38 @@
-import React from "react";
+import React, { useState } from "react";
 import { useTheme } from "../../context/ThemeContext";
+import { useUser } from "../../context/UserContext";
+import firebase, { database } from "../../firebase/firebaseUtils";
 
 import "./ChatInput.style.scss";
 
-export default function ChatInput({ onSubmitFunction, handleChange }) {
+export default function ChatInput({ chatID }) {
   const { appTheme } = useTheme();
+  const { userStorage } = useUser();
+
+  const [msg, setMsg] = useState("");
+
+  const sendMessage = (event) => {
+    event.preventDefault();
+    if (msg.trim()) {
+      database.collection(`room-list/${chatID}/messages`).add({
+        text: msg,
+        sentAt: new Date(),
+        userName: userStorage.name,
+        messageColor: userStorage.color,
+        userID: userStorage.id,
+      });
+      setMsg("");
+    }
+  };
+  const handleChatChange = (event) => {
+    setMsg(event.target.value);
+  };
 
   return (
-    <form className="chat__form" onSubmit={onSubmitFunction}>
+    <form className="chat__form" onSubmit={sendMessage}>
       <input
-        onChange={handleChange}
+        value={msg}
+        onChange={handleChatChange}
         type="text"
         className={`chat__input chat__input--${appTheme}`}
       />
--- src/pages/rooms/Rooms.js
+++ src/pages/rooms/Rooms.js
@@ -1,4 +1,3 @@
-import React, { useEffect, useState } from "react";
 import AppWindow from "../../components/app-window/AppWindow";
 import languages_data from "../../utils/language";
 import { useTheme } from "../../context/ThemeContext";
@@ -9,33 +8,24 @@ import ChatList from "../../components/chat-list/ChatList";
 
 import "../Pages.style.scss";
 import ChatInput from "../../components/chat-input/ChatInput";
-import Modal from "../modal/Modal";
-import { database } from "../../firebase/firebaseUtils";
-import firebase from "firebase";
+
+import { Redirect } from "react-router-dom";
+import { useUser } from "../../context/UserContext";
 
 export default function Rooms() {
   const { language } = useTheme();
+  const { userStorage } = useUser();
   const { id } = useParams();
-  const [msg, setMsg] = useState("");
 
-  function sendMessage(e) {
-    e.preventDefault();
-    database.collection(`room-list/${id}/messages`).add({
-      text: msg,
-      sentAt: firebase.firestore.FieldValue.serverTimestamp(),
-    });
-    setMsg("");
-  }
-  return (
+  return !userStorage ? (
+    <Redirect to="/login" />
+  ) : (
     <div className="rooms-page">
       <AppWindow title={languages_data[language].rooms}>
         <div className="rooms-page__content">
           <RoomList />
           {id ? <ChatList chatID={id} /> : <h1>BOS ROOM TEST</h1>}
-          <ChatInput
-            handleChange={(e) => setMsg(e.target.value)}
-            onSubmitFunction={sendMessage}
-          />
+          <ChatInput chatID={id} />
         </div>
       </AppWindow>
       {/* <Modal /> */}
