1d6ed6b6e2868403839bade304e293e4d6a65404
PDOUU <58252790+pdouu@users.noreply.github.com>
2021-08-13 18:19:24 +0300

    change window class name

--- src/components/app-window/AppWindow.js
+++ src/components/app-window/AppWindow.js
@@ -1,14 +1,17 @@
-import React from "react";
+import React, { memo } from "react";
 import { useTheme } from "../../context/ThemeContext";
 
 import "./AppWindow.style.scss";
-export default function AppWindow({ title, children }) {
+function AppWindow({ title, children }) {
   const { appTheme } = useTheme();
 
   return (
     <div className="window">
       <div className={`window__bar window__bar--${appTheme}`}>{title}</div>
-      <div className="window__content window__content--retro">{children}</div>
+      <div className={`window__content window__content--${appTheme}`}>
+        {children}
+      </div>
     </div>
   );
 }
+export default memo(AppWindow);
