9d974d410a0aa0e61a60c51cb24316fb8026362f
PDOUU <58252790+pdouu@users.noreply.github.com>
2021-08-13 18:18:18 +0300

    refactor route controls

--- src/App.js
+++ src/App.js
@@ -4,22 +4,41 @@ import { Switch, Route, Redirect } from "react-router-dom";
 import "./App.scss";
 import Login from "./pages/login/Login";
 import Rooms from "./pages/rooms/Rooms";
+import { useUser } from "./context/UserContext";
+
+const pageComponents = {
+  login: <Login />,
+  rooms: <Rooms />,
+};
+
+//Check if user exists then navigate to destionation path
+function redirectUser(user, routePath, destinationPath) {
+  return user ? (
+    <Redirect to={`/${destinationPath}`} />
+  ) : (
+    pageComponents[routePath]
+  );
+}
 
 function App() {
   //Theme Context
   const { appTheme } = useTheme();
+  const { userStorage } = useUser();
 
   return (
     <div className={`app app__background--${appTheme}`}>
       <Switch>
+        <Route exact path="/">
+          <Redirect to={`/${userStorage ? "rooms" : "login"}`} />
+        </Route>
         <Route exact path="/login">
-          <Login />
+          {redirectUser(userStorage, "login", "rooms")}
         </Route>
         <Route exact path="/rooms/:id">
-          <Rooms />
+          {redirectUser(!userStorage, "rooms", "login")}
         </Route>
         <Route exact path="/rooms/">
-          <Rooms />
+          {redirectUser(!userStorage, "rooms", "login")}
         </Route>
         <Route path="*">
           <Login />
